d95caecffc6ed3733cc466d6bcf442ff9120ab94
veronicasalt <veronica49saltanov@gmail.com>
2026-05-25 16:35:22 -0700

    confusion matrix

--- Lab5/neuralnetwork.py
+++ Lab5/neuralnetwork.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 fashion_mnist = keras.datasets.fashion_mnist 
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data() 
 x_train, x_test = x_train / 255.0, x_test / 255.0 # normalize the pixel values to be in [0, 1] 
@@ -75,4 +76,26 @@ history = model.fit(
 
 print("Training complete.")
 
+# eval test set
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
 
+print(f"Test Accuracy: {test_accuracy * 100:.2f}%")
+
+y_pred_probs = model.predict(x_test) # predicting, 10k x 10 matrix, 10 prob scores
+
+# pick highest prob in each row, 
+# class labels
+y_pred = np.argmax(y_pred_probs, axis=1)
+
+
+# confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion MAtrix:")
+print(cm)
+
+    # visuals
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classlabels)
+disp.plot(xticks_rotation=45)
+plt.title("Confusion Matrix - Fashion MNIST")
+plt.tight_layout()
+plt.show()
